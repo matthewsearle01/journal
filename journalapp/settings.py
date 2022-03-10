780d2b4f16f0bd7dd5289d42f0010f1641519486
Matt Searle <mattsearle@Matts-MacBook-Pro-2.local>
2022-03-10 12:37:25 +0000

    created heroku test branch

--- journalapp/settings.py
+++ journalapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-f4y3k-&*n04l=n4etb^ibr$n5))#ha%i9ed_djp+5ef6)kupip'
+SECRET_KEY = 'django-insecure-f4y3k-&*n04l=n4etb^ibr$n5))#ha%i9ed_djp+5ef6)kupip'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,5 +131,4 @@ DJANGO_ICONS = {
         "edit": {"name": "far fa-pencil"},
     },
 }
-import django_heroku
 django_heroku.settings(locals())
