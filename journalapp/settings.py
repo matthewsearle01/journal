2d1f6611219313d84c028cc0cf855c31d6d817db
Matt Searle <mattsearle@matts-mbp-2.home>
2022-03-15 11:41:39 +0000

    moved db from sqlite to mysql

--- journalapp/settings.py
+++ journalapp/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'journalapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # <-- UPDATED line
+        'NAME': 'journaldb',                 # <-- UPDATED line
+        'USER': 'root',                     # <-- UPDATED line
+        'PASSWORD': 'Sycamore41',              # <-- UPDATED line
+        'HOST': 'localhost',                # <-- UPDATED line
+        'PORT': '3306',
     }
 }
 
